43c5e58f8332265f37e564c72492ca5b2dbbf3bc
Evelyn Santoso <velyn.es24@gmail.com>
2024-07-12 09:48:55 +0700

    Initial Commit

---
+++ githubpractice/githubpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubpracticeApp.swift
+//  githubpractice
+//
+//  Created by Evelyn Santoso on 12/07/24.
+//
+
+import SwiftUI
+
+@main
+struct githubpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
